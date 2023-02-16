be7afe93ff03830da1b0b4a5e28e5f22a585991a
jashgada <jashgada93@gmail.com>
2023-02-16 15:46:58 -0500

    add google log in

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './home/Home-Page';
 import Explore from './explore/Explore-Page';
 import Account from './account/Account';
@@ -13,20 +14,35 @@ import Favorites from './favorites/Favorites-Page';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-     <BrowserRouter>
-    <Routes>
-        <Route path="/" element={<App />} >
-            <Route path="/" element={<Home/>}/>
-            <Route path="/explore" element={<Explore/>}/>
-            <Route path="/account" element={<Account/>}/>
-            <Route path="/news" element={<News/>}/>
-            <Route path="/favorites" element={<Favorites/>}/>
-            {/* <Route path="/" element={user?.email ? <Navigate to="/home" /> : <Landing />} />*/}
-        </Route>
-    </Routes>
-  </BrowserRouter>
-  </React.StrictMode>
+  <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+      <React.StrictMode>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<App />} >
+              <Route path="/" element={<Home/>}/>
+              <Route path="/explore" element={<Explore/>}/>
+              <Route path="/account" element={<Account/>}/>
+              <Route path="/news" element={<News/>}/>
+              <Route path="/favorites" element={<Favorites/>}/>
+            </Route>
+          </Routes>
+        </BrowserRouter> 
+      </React.StrictMode>
+    </GoogleOAuthProvider>,
+  // <React.StrictMode>
+  //    <BrowserRouter>
+  //   <Routes>
+  //       <Route path="/" element={<App />} >
+  //           <Route path="/" element={<Home/>}/>
+  //           <Route path="/explore" element={<Explore/>}/>
+  //           <Route path="/account" element={<Account/>}/>
+  //           <Route path="/news" element={<News/>}/>
+  //           <Route path="/favorites" element={<Favorites/>}/>
+  //       </Route>
+  //   </Routes>
+  // </BrowserRouter> 
+  // <App/>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
