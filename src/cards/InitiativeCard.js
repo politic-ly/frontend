a4da6dfdccfd6a4c2f7df5115d708281c00661f2
jashgada <jashgada93@gmail.com>
2023-04-04 16:23:50 -0400

    pr req changes

--- src/cards/InitiativeCard.js
+++ src/cards/InitiativeCard.js
@@ -60,56 +60,54 @@ const InitiativeCard = ({
         background: `linear-gradient(0deg, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${img})`,
       }}
     >
-      <CardActionArea>
-        <div className="initiativeCard--text">
-          <Link style={{ textDecoration: "none" }} to={"/initiative/" + id}>
-            <p className="initiativeCard--location">{location.toUpperCase()}</p>
-            <div className="initiativeCard--title">
-              <h2>
-                <span>{title}</span>
-              </h2>
-              <p>
-                <span>{subtitle}</span>
-              </p>
-            </div>
-          </Link>
-        </div>
-        <div className="initiativeCard--function">
-          {favorited ? (
-            <Favorite
-              className="initiativeCard--favorited"
-              onClick={(e) => {
-                e.stopPropagation();
-                toggleFavorite();
-              }}
-            />
-          ) : (
-            <FavoriteBorderOutlined
-              className="initiativeCard--favorited"
-              onClick={(e) => {
-                toggleFavorite();
-              }}
-            />
-          )}
-          <div className="initiativeCard--volunteers">
-            <p>{volunteerList.length} volunteers</p>
-            <div className="initiativeCard--volunteerList">
-              <AvatarGroup max={10}>
-                {volunteerList.map((vol, index) => {
-                  return (
-                    <Avatar
-                      key={index}
-                      src={vol.profileImg}
-                      alt={vol.username}
-                      sx={{ width: 24, height: 24 }}
-                    />
-                  );
-                })}
-              </AvatarGroup>
-            </div>
+      <div className="initiativeCard--text">
+        <Link style={{ textDecoration: "none" }} to={"/initiative/" + id}>
+          <p className="initiativeCard--location">{location.toUpperCase()}</p>
+          <div className="initiativeCard--title">
+            <h2>
+              <span>{title}</span>
+            </h2>
+            <p>
+              <span>{subtitle}</span>
+            </p>
+          </div>
+        </Link>
+      </div>
+      <div className="initiativeCard--function">
+        {favorited ? (
+          <Favorite
+            className="initiativeCard--favorited"
+            onClick={(e) => {
+              e.stopPropagation();
+              toggleFavorite();
+            }}
+          />
+        ) : (
+          <FavoriteBorderOutlined
+            className="initiativeCard--favorited"
+            onClick={(e) => {
+              toggleFavorite();
+            }}
+          />
+        )}
+        <div className="initiativeCard--volunteers">
+          <p>{volunteerList.length} volunteers</p>
+          <div className="initiativeCard--volunteerList">
+            <AvatarGroup max={10}>
+              {volunteerList.map((vol, index) => {
+                return (
+                  <Avatar
+                    key={index}
+                    src={vol.profileImg}
+                    alt={vol.username}
+                    sx={{ width: 24, height: 24 }}
+                  />
+                );
+              })}
+            </AvatarGroup>
           </div>
         </div>
-      </CardActionArea>
+      </div>
     </Card>
   );
 };
